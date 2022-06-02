4ccb3a241aed258e5a5023008a0e3b4b55fb62c1
SAK <99003095+SAK1437@users.noreply.github.com>
2022-06-02 10:32:45 +0000

    code

---
+++ ActivitySet#1/problem#03.py
@@ -0,0 +1,5 @@
+
+hours = input('enter your number of hours: ')
+rate_of_hours = input('enter the rate: ')
+gross_payment = float(hours) * float(rate_of_hours)
+print(f"Rs.{gross_payment} is gross pay for {hours} hours and {rate_of_hours} rate ")
