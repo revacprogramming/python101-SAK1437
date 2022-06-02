9a2ae6c9f5b568261010f4d98f5e6d86a5120801
SAK <99003095+SAK1437@users.noreply.github.com>
2022-06-02 17:47:27 +0000

    code10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,13 @@
-# Dictionaries
 
-filename = "dataset/mbox-short.txt"
+num_of_days = dict()
+file_name = input("Enter name of file: ")
+if file_name == 'p':
+    file_exe = open('progrm8.txt')
+    for lines in file_exe:
+        if lines.startswith("From "):
+            words = lines.split()
+            if words[2] not in num_of_days:
+                num_of_days[words[2]] = 1
+            else:
+                num_of_days[words[2]] += 1
+print(num_of_days)
