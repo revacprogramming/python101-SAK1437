abd2ad02c01d458ea8aa4a986380a14287e05d9e
SAK <99003095+SAK1437@users.noreply.github.com>
2022-06-02 17:55:48 +0000

    code11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,17 @@
-# Tuples
 
-filename = "dataset/mbox-short.txt"
+reqdlist = []
+senders = dict()
+text_name = input("Enter the name of file: ")
+if text_name == "p":
+    text_exe = open('progrm8.txt')
+    for lines in text_exe:
+        if lines.startswith('From '):
+            words = lines.split()
+            if words[1] not in senders:
+                senders[words[1]] = 1
+            else:
+                senders[words[1]] += 1
+for tpl in senders:
+    reqdlist.append((senders[tpl],tpl))
+reqdlist.sort()
+print(reqdlist[-1])
