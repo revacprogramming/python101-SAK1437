6b201556beca7201d53911c0e227a79d25716d01
SAK <99003095+SAK1437@users.noreply.github.com>
2022-06-02 10:50:21 +0000

    code8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,10 @@
-# Files
 
-filename = "dataset/mbox-short.txt"
+file_name = input("Enter name of file: ")
+if file_name == 'progrm8.txt':
+    file_exe = open('progrm8.txt')
+    for lines in file_exe:
+        capitalized = lines.upper()
+        print(capitalized)
+else:
+    print("Invalid file name")
+file_exe.close()
