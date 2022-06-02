83231c1a42238c73248819d0db3a6645b6232f38
SAK <99003095+SAK1437@users.noreply.github.com>
2022-06-02 10:39:37 +0000

    code

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,9 @@
-# Conditional Execution
 
-hrs = input("Enter hours? ")
+hours = input("Enter the number of working hours of employee: ")
+rate = input("Enter the payment per working hour: ")
+payment = float(hours) * float(rate)
+extra_payment = (float(hours)-40)*(float(rate)*0.5)
+if float(hours)>40:
+  print(payment + extra_payment)
+else:
+  print(payment)
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,11 @@
-# Functions
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+def computepay(h,r) :
+    payment = float(h)*float(r)
+    extra_payment = (float(h)-40)*(float(rate)*0.5)
+    if float(h)>40:
+        return payment+extra_payment
+    return payment
+hours = float(input('Enter the hours: '))
+rate = float(input('Enter the rate: '))
+p = computepay(hours,rate)
+print(f'Pay {p}')
