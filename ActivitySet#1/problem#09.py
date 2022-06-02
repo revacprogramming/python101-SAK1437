0201fa1dc4df436661d2f119bee22f28a3300e9d
SAK <99003095+SAK1437@users.noreply.github.com>
2022-06-02 10:54:05 +0000

    code9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,13 @@
-# Lists
 
-filename = "dataset/romeo.txt"
+file_name = input("Enter name of file: ")
+uniques=[ ]
+if file_name == 'romeo.txt':
+    file_exe = open('progrm9.txt')
+    for line in file_exe:
+        uniq = line.split()
+        for word in uniq:
+            if not(word in uniques):
+                uniques.append(word)
+uniques.sort()
+print(len(uniques))
+print(uniques)
