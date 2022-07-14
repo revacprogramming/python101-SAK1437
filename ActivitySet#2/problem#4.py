c602c62928f953762949a215f3af977fc34aa62c
SAK <99003095+SAK1437@users.noreply.github.com>
2022-07-14 10:38:32 +0000

    Set2-code-4

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,26 +1,29 @@
 
 
-def get_cs():
-    """get string input"""
-
-
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
-def lot_to_cs(lot):
-    """convert list of strings to connected string"""
-
-
-def main():
-    cs=get_cs()
-
-    lot=cs_to_lot(cs)  # convert connect string to list of tuples
-    print(lot)
-
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
-
-
-if __name__ == '__main__':
-    main()
+reqd_data = ['system','database','username','passwd'] 
+ def get_cs() : 
+     cs = input("Enter the connected string: ") 
+     return cs 
+ def cs_to_lot(cs) : 
+     tupl_list = [] 
+     n=0 
+     while n<len(reqd_data) : 
+         tupl_list.append((reqd_data[n],cs[n])) 
+         n +=1 
+     return tupl_list 
+ def lot_to_cs(lot) : 
+     n=0 
+     cs='' 
+     while n< 4 : 
+         element = lot[n] 
+         cs += (f'{element[0]} = {element[1]}; ') 
+         n += 1 
+     return cs 
+ def main() : 
+     cs = get_cs() 
+     lot = cs_to_lot(cs) 
+     print(lot) 
+     cs = lot_to_cs(lot) 
+     print(cs) 
+ if _name=='main_': 
+     main()
