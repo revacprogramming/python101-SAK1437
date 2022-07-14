04fad3279a1cd015c55253f6ba4986e078bd10b6
SAK <99003095+SAK1437@users.noreply.github.com>
2022-07-14 10:22:56 +0000

    Set2-code-2

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,19 @@
 
-def add(a, b):
-    pass  # ...
-
-
-def output(a, b, sum):
-    pass  # ...
-
-
-def main():
-    a, b = input_two_numbers()
-    sum = add(a, b)
-
-    output(a, b, sum)
-
-
-if __name__ == '__main__':
-    main()
+def in_put() : 
+         x = float(input("enter the number: ")) 
+         return x 
+          
+ def add(a,b) : 
+     return a+b 
+          
+ def output(a,b,sum) : 
+     print(f"{a} + {b} = {sum}") 
+          
+ def main(): 
+         a = in_put() 
+         b = in_put() 
+         sum = add(int(a),int(b)) 
+         output(a,b,sum) 
+  
+ if _name_ == '_main_' : 
+     main()
