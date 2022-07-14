64cf73163fd6729a8728b657a4b43c1abba3e531
SAK <99003095+SAK1437@users.noreply.github.com>
2022-07-14 10:44:34 +0000

    Set2-code-5

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,26 +1,31 @@
 
 
+#s=system;d=database;u=username;p=ord
 def get_cs():
-    """get string input"""
-
+    cs = input("enter your value")
+    return cs
 
 def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
-
+    reqd_dict = dict()
+    for ele in cs:
+        ele = cs.split(';')
+    for x in ele:
+        word = x.split('=')
+        reqd_dict[word[0]] = word[1]
+    return reqd_dict
 
 def dict_to_cs(d):
-    """convert a dictionary to connect string"""
-
+    reqd_cs = ''
+    for i in d:
+        reqd_cs+=(f"{i} = {d[i]} ; ")
+    return reqd_cs
 
 def main():
     cs = get_cs()
-
-    d = cs_to_dict(cs) # convert connect string to a dictionary
+    d = cs_to_dict(cs) 
     print(d)
-
     cs = dict_to_cs(d)
     print(cs)
 
-
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
