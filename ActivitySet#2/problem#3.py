c85eecd0040117dba39163900d8a8759feed5ac4
SAK <99003095+SAK1437@users.noreply.github.com>
2022-07-14 10:29:39 +0000

    Set2-code-3

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,19 +1,18 @@
 
-
-def get_cs():
-    """get string input"""
-
-
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
-def main():
-    cs = get_cs()
-
-    lot = cs_to_lot(cs)
-    print(lot)
-
-
-if __name__ == '__main__':
-    main()
+reqd_data = ['system','database','username','password'] 
+ def get_cs() : 
+     cs= input("enter the values: ") 
+     return cs  
+ def cs_to_lot(cs) : 
+     tupl_list = [] 
+     n=0 
+     while n<len(reqd_data) : 
+         tupl_list.append((reqd_data[n],cs[n])) 
+         n+=1 
+     return tupl_list 
+ def main() : 
+     cs = get_cs() 
+     lot = cs_to_lot(cs) 
+     print(lot) 
+ if _name_ == '_main_': 
+     main()
