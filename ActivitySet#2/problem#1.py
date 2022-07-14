2acdd04c99543ae69426e6a2262957c350b9e324
SAK <99003095+SAK1437@users.noreply.github.com>
2022-07-14 10:16:44 +0000

    Set2-code-1

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,2 +1,10 @@
 
 
+def add(a,b) : 
+     return a+b 
+ def main() : 
+     a = int(input("Enter first number: ")) 
+     b = int(input("Enter second number: ")) 
+     c = add(a,b) 
+     print(f" Sum of {a} and {b} is {c} ") 
+ main()
